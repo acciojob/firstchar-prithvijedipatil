bba3d9548959d02e5614bc26cc0b2a399436660c
prithvijedipatil <prithvipatil09@gmail.com>
2023-09-11 12:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstChar(text) {
   // your code here
+	if(text.length===0){
+		return '';
+	}
+	else if(text[0]===' '){
+		return '';
+	}
+	else {
+		return text[0];
+	}
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(text)); 
